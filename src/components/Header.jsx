49bb8c2a485de7b35fa460c40ae1e201a5b2c24b
Yana Kochenova <yana.kochenova@mail.ru>
2023-10-02 10:40:44 +0400

    added new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://phonoteka.org/uploads/posts/2021-04/1619706269_22-phonoteka_org-p-znachok-instagram-bez-fona-dlya-fotoshopa-22.png'></img>
+      </header>
+    )
+    }
+
+    export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://w.forfun.com/fetch/da/daf8eb568fea522f6701fb9c66378cdc.jpeg'></img>
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              Post1
+            </div>
+            <div>
+              Post2
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
